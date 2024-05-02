c15d52ec896538ae18abeeab0ab988dc576af927
marat569 <marat569@gmail.com>
2024-05-01 23:06:20 -0400

    Update main.lua

--- hdr-dx11/scripts/main.lua
+++ hdr-dx11/scripts/main.lua
@@ -33,7 +33,7 @@ function Loop()
 ---	 ExecCmd("r.HDR.Display.OutputDevice 5")
 ---	 ExecCmd("r.HDR.Display.ColorGamut 2")
 ---	 ExecCmd("r.HDR.UI.CompositeMode 1")
----	 ExecCmd("r.HDR.UI.Level 1.5")
+---	 ExecCmd("r.HDR.UI.Level 1.0")
 
 	
 end
@@ -50,7 +50,7 @@ function Init()
 	ExecCmd("r.HDR.Display.OutputDevice 5")
 	ExecCmd("r.HDR.Display.ColorGamut 2")
     ExecCmd("r.HDR.UI.CompositeMode 1")
-	ExecCmd("r.HDR.UI.Level 1.5")
+	ExecCmd("r.HDR.UI.Level 1.0")
 
     LoopAsync(500, function()
         Loop()
